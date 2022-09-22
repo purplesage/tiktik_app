516a5d14bf5842b0c85d5cf46e42e21fe131550b
David Mendoza <hectormza21@gmail.com>
2022-09-22 14:28:54 -0400

    main content scripting / styling + google auth

--- next.config.js
+++ next.config.js
@@ -2,6 +2,9 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-}
+  images: {
+    domains: ["w7.pngwing.com"],
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
