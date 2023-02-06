85104fc7704fc7fb34704eadfce18f9cf0eb17f4
purpleSage <hectormza21@gmail.com>
2023-02-06 17:21:32 -0400

    fix search state.

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,104 +1,104 @@
-import React, { useState, useEffect } from "react";
-import Image from "next/image";
-import Link from "next/link";
-import Router, { useRouter } from "next/router";
-import { AiOutlineLogout } from "react-icons/ai";
-import { BiSearch } from "react-icons/bi";
-import { IoMdAdd } from "react-icons/io";
-import { GoogleLogin, googleLogout } from "@react-oauth/google";
-import Logo from "../utils/tiktik-logo.png";
-import { createOrGetUser } from "../utils";
-import useAuthStore from "../store/authStore";
+import React, { useState, useEffect } from 'react';
+import Image from 'next/image';
+import Link from 'next/link';
+import Router, { useRouter } from 'next/router';
+import { AiOutlineLogout } from 'react-icons/ai';
+import { BiSearch } from 'react-icons/bi';
+import { IoMdAdd } from 'react-icons/io';
+import { GoogleLogin, googleLogout } from '@react-oauth/google';
+import Logo from '../utils/tiktik-logo.png';
+import { createOrGetUser } from '../utils';
+import useAuthStore from '../store/authStore';
 
 const Navbar = () => {
-  const { userProfile, addUser, removeUser } = useAuthStore();
-  const [searchValue, setSearchValue] = useState("second");
-  const router = useRouter();
-  const handleSearch = (e: { preventDefault: () => void }) => {
-    e.preventDefault();
+	const { userProfile, addUser, removeUser } = useAuthStore();
+	const [searchValue, setSearchValue] = useState('');
+	const router = useRouter();
+	const handleSearch = (e: { preventDefault: () => void }) => {
+		e.preventDefault();
 
-    if (searchValue) {
-      Router.push(`/search/${searchValue}`);
-    }
-  };
-  return (
-    <div className="w-full flex justify-between items-center border-b-2 borer-gray-200 py-2 px-4">
-      <Link href="/">
-        <div className="w-[100px] md:w[130px]">
-          <Image
-            className="cursor-pointer"
-            src={Logo}
-            alt="TikTik"
-            layout="responsive"
-          />
-        </div>
-      </Link>
-      <div className="relative hidden md:block">
-        <form
-          className="absolute md:static top-10 -left-20 bg-white"
-          onSubmit={handleSearch}
-        >
-          <input
-            className="bg-primary p-3 md:text-md font-medium border-2 border-gray-100 focus:outline-none focus:border-2 focus:border-gray-300 w-[300px] md:w-[350px] rounded-full"
-            type="text"
-            value={searchValue}
-            onChange={(e) => setSearchValue(e.target.value)}
-            placeholder="Search accounts and Videos"
-          />
-          <button
-            onClick={handleSearch}
-            className="absolute md:right-5 top-4 border-l-2 border-gray-300 pl-4 text-2xl text-gray-300 "
-          >
-            <BiSearch />
-          </button>
-        </form>
-      </div>
-      <div>
-        {userProfile ? (
-          <div className="flex gap-5 md:gap-10">
-            <Link href="/upload">
-              <button className="border-2 px-2 md:px-4 text-md font-semibold flex items-center gap-2">
-                <IoMdAdd className="text-xl" />{" "}
-                <span className="hidden md:block">Upload</span>
-              </button>
-            </Link>
-            {userProfile.image && (
-              <Link href="/">
-                <>
-                  <Image
-                    width={40}
-                    height={40}
-                    className="rounded-full cursor-pointer"
-                    src={userProfile.image}
-                    alt="profile phoot"
-                  />
-                </>
-              </Link>
-            )}
-            <button type="button">
-              <AiOutlineLogout
-                color="red"
-                fontSize={30}
-                onClick={() => {
-                  googleLogout();
-                  removeUser();
-                }}
-              />
-            </button>
-          </div>
-        ) : (
-          <GoogleLogin
-            onSuccess={(response) => {
-              createOrGetUser(response, addUser);
-            }}
-            onError={() => {
-              console.log("Error");
-            }}
-          />
-        )}
-      </div>
-    </div>
-  );
+		if (searchValue) {
+			Router.push(`/search/${searchValue}`);
+		}
+	};
+	return (
+		<div className="w-full flex justify-between items-center border-b-2 borer-gray-200 py-2 px-4">
+			<Link href="/">
+				<div className="w-[100px] md:w[130px]">
+					<Image
+						className="cursor-pointer"
+						src={Logo}
+						alt="TikTik"
+						layout="responsive"
+					/>
+				</div>
+			</Link>
+			<div className="relative hidden md:block">
+				<form
+					className="absolute md:static top-10 -left-20 bg-white"
+					onSubmit={handleSearch}
+				>
+					<input
+						className="bg-primary p-3 md:text-md font-medium border-2 border-gray-100 focus:outline-none focus:border-2 focus:border-gray-300 w-[300px] md:w-[350px] rounded-full"
+						type="text"
+						value={searchValue}
+						onChange={(e) => setSearchValue(e.target.value)}
+						placeholder="Search accounts and Videos"
+					/>
+					<button
+						onClick={handleSearch}
+						className="absolute md:right-5 top-4 border-l-2 border-gray-300 pl-4 text-2xl text-gray-300 "
+					>
+						<BiSearch />
+					</button>
+				</form>
+			</div>
+			<div>
+				{userProfile ? (
+					<div className="flex gap-5 md:gap-10">
+						<Link href="/upload">
+							<button className="border-2 px-2 md:px-4 text-md font-semibold flex items-center gap-2">
+								<IoMdAdd className="text-xl" />{' '}
+								<span className="hidden md:block">Upload</span>
+							</button>
+						</Link>
+						{userProfile.image && (
+							<Link href="/">
+								<>
+									<Image
+										width={40}
+										height={40}
+										className="rounded-full cursor-pointer"
+										src={userProfile.image}
+										alt="profile phoot"
+									/>
+								</>
+							</Link>
+						)}
+						<button type="button">
+							<AiOutlineLogout
+								color="red"
+								fontSize={30}
+								onClick={() => {
+									googleLogout();
+									removeUser();
+								}}
+							/>
+						</button>
+					</div>
+				) : (
+					<GoogleLogin
+						onSuccess={(response) => {
+							createOrGetUser(response, addUser);
+						}}
+						onError={() => {
+							console.log('Error');
+						}}
+					/>
+				)}
+			</div>
+		</div>
+	);
 };
 
 export default Navbar;
